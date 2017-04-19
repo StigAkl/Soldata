38dc5dd3d3aed30e34109934b7c235deae247f03
Stigiboy <stg@hotmail.no>
2017-04-19 14:25:50 +0200

    Init

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" type="text/css" href="design/default.css">
+</head>
+<body>
+
+<div class="title">Velkommen til soldata.no</div>
+
+</body>
+</html>
